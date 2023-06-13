7a1b05a9835b213b9ce6cad371c922f7366b7620
guptasonalii <sonaligupta1498@gmail.com>
2023-06-13 18:30:30 +0530

    react practice

---
+++ react_app/src/App.js
@@ -0,0 +1,47 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import MyfirstComponent from "./Components/MyfirstComponent";
+import About from "./Components/About";
+import Jsx from "./Components/Jsx";
+import Jsx1 from "./Components/Jsx1";
+import Props from "./Components/Props";
+import CommonProp from "./Components/CommonProp";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center mt-4 bg-dark text-white">React Tutorial</h1>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/" className="text-dark text-decoration-none">MyFirstComponent</Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-dark text-decoration-none"> About </Link>
+          </li>
+          <li>
+            <Link to="/jsx" className="text-primary text-decoration-none"> Jsx </Link>
+          </li>
+          <li>
+            <Link to="/jsx1" className="text-primary text-decoration-none"> Display Current Date and Time </Link>
+          </li>
+          <li>
+          <Link to="/Props" className="text-primary text-decoration-none">Props </Link>
+
+          </li>
+
+        </ul>
+
+        <Routes>
+          <Route path="/" Component={MyfirstComponent} />
+          <Route path="/about" element={<About />} />
+          <Route path="/jsx" element={<Jsx/>}/>
+          <Route path="/jsx1" element={<Jsx1/>}/>
+          <Route path="/Props" element={<Props/>}/>
+
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
---
+++ react_app/src/Components/Jsx.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Jsx = () => {
+  return (
+    <div>
+      <h1>JSX</h1>
+      <h5>Hedy Lamarr's Todos</h5>
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo" 
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    
+    </div>
+  )
+}
+
+export default Jsx
+
